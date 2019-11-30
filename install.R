b8fc908033a64839e1a28d21e5860835d9c3bdc5
Kevin Wang <kevin.wang@sydney.edu.au>
2019-11-30 23:39:34 +1100

    initiate with copied files

---
+++ install.R
@@ -0,0 +1,24 @@
+if ("BiocManager" %in% rownames(installed.packages()))
+	remove.packages("BiocManager")
+
+install.packages("BiocManager", repos="https://cran.rstudio.com")
+#install.packages("devtools", repos="https://cran.rstudio.com")
+#devtools::install_github("Bioconductor/BiocManager")
+library(BiocManager)
+
+if(BiocManager::version() != "3.10"){
+    BiocManager::install(version="3.10",
+                         update=TRUE, ask=FALSE)
+}
+
+std_pkgs <- c()
+
+gh_pkgs <- c("PengyiYang/PhosR")
+
+builtins <- c(std_pkgs, gh_pkgs)
+
+for (builtin in builtins)
+    if (!suppressWarnings(require(builtin, character.only=TRUE)))
+        suppressWarnings(BiocManager::install(builtin,
+                                              version="3.10",
+                                              update=TRUE, ask=FALSE))
